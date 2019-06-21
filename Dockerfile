e342088315481e5ff7f07af56d72c1690a92bfe2
Ľubomír Kučera <lubomir.kucera.jr@gmail.com>
2019-06-21 12:14:12 +0200

    Initial code import

---
+++ Dockerfile
@@ -0,0 +1,95 @@
+# Python 3.7 is currently not suppored. See https://github.com/taigaio/taiga-back/issues/1328.
+FROM python:3.6-alpine3.10
+RUN set -ex; \
+    \
+    apk add --no-cache \
+        libjpeg-turbo \
+        libpq \
+        libxslt \
+        mailcap \
+        pcre \
+    ; \
+    \
+    apk add --no-cache --virtual .build-deps \
+        gcc \
+        linux-headers \
+        musl-dev \
+        pcre-dev \
+    ; \
+    \
+    pip install --no-cache-dir 'uWSGI>=2.0,<2.1'; \
+    addgroup -g 101 -S taiga; \
+    adduser -D -H -g taiga -G taiga -s /sbin/nologin -S -u 101 taiga; \
+    \
+    find /usr/local -depth \
+        \( \
+            \( -type d -a \( -name test -o -name tests \) \) \
+            -o \
+            \( -type f -a \( -name '*.pyc' -o -name '*.pyo' \) \) \
+        \) -exec rm -rf '{}' +; \
+    apk del .build-deps; \
+    rm -rf /var/cache/apk/*
+ENV TAIGA_VERSION=4.2.7 \
+    TAIGA_BACK_SHA256SUM=a25f4c880fceb6552c0ffd027ec5345a54a13b2a1379e586efd1d6de3bbf42a6 \
+    TAIGA_FRONT_SHA256SUM=b0f94806455b811848b3e10df8d3f63a0a741da83296d267fe83b19767597ee7
+RUN set -ex; \
+    \
+    apk add --no-cache --virtual .build-deps \
+        gcc \
+        gettext \
+        libffi-dev \
+        libjpeg-turbo-dev \
+        libxslt-dev \
+        musl-dev \
+        postgresql-dev \
+        zlib-dev \
+    ; \
+    \
+    wget -q -O taiga-back.tar.gz \
+        https://github.com/taigaio/taiga-back/archive/${TAIGA_VERSION}.tar.gz; \
+    echo "${TAIGA_BACK_SHA256SUM}  taiga-back.tar.gz" | sha256sum -c; \
+    tar -xzf taiga-back.tar.gz; \
+    rm -r taiga-back.tar.gz taiga-back-${TAIGA_VERSION}/tests; \
+    mv taiga-back-${TAIGA_VERSION} /opt/taiga-back; \
+    cd /opt/taiga-back; \
+    sed -i '/^gunicorn==/d' requirements.txt; \
+    pip install --no-cache-dir -r requirements.txt; \
+    ./manage.py compilemessages; \
+    mkdir -p /etc/opt/taiga-back /srv/taiga-back/media /srv/taiga-back/static; \
+    cd -; \
+    \
+    wget -q -O taiga-front-dist.tar.gz \
+        https://github.com/taigaio/taiga-front-dist/archive/${TAIGA_VERSION}-stable.tar.gz; \
+    echo "${TAIGA_FRONT_SHA256SUM}  taiga-front-dist.tar.gz" | sha256sum -c; \
+    tar -xzf taiga-front-dist.tar.gz; \
+    mv taiga-front-dist-${TAIGA_VERSION}-stable/dist /opt/taiga-front; \
+    rm -r taiga-front-dist.tar.gz taiga-front-dist-${TAIGA_VERSION}-stable; \
+    mkdir -p /etc/opt/taiga-front; \
+    # Removes origin from "api" URL. By default, the API is served on port
+    # 8080. Also, the URL doesn't have to be absolute, so this make the
+    # default configuration more generic.
+    sed -i 's|http://localhost:8000||' /opt/taiga-front/conf.example.json; \
+    mv /opt/taiga-front/conf.example.json /etc/opt/taiga-front/conf.json; \
+    ln -s /etc/opt/taiga-front/conf.json /opt/taiga-front/conf.json; \
+    \
+    chmod -R go-w /opt/taiga-back /opt/taiga-front; \
+    \
+    find /opt/taiga-back /usr/local -depth \
+        \( \
+            # taiga-back requires django.test and there's no other test directory
+            \( -type d -a -name tests \) \
+            -o \
+            \( -type f -a \( -name '*.pyc' -o -name '*.pyo' \) \) \
+        \) -exec rm -rf '{}' +; \
+    apk del .build-deps; \
+    rm -rf /var/cache/apk/*
+COPY files /
+WORKDIR /opt/taiga-back
+ENV \
+    # See https://uwsgi-docs.readthedocs.io/en/latest/HTTP.html.
+    UWSGI_HTTP=:8080 \
+    # See https://uwsgi-docs.readthedocs.io/en/latest/StaticFiles.html#offloading and
+    # https://uwsgi-docs.readthedocs.io/en/latest/OffloadSubsystem.html.
+    UWSGI_OFFLOAD_THREADS=1
+ENTRYPOINT ["entrypoint"]
+EXPOSE 8080
---
+++ files/etc/opt/taiga-back/settings.py
@@ -0,0 +1,203 @@
+# For the complete list of possible settings and their documentation, see
+# https://docs.djangoproject.com/en/1.11/ref/settings/ and
+# https://github.com/taigaio/taiga-back/tree/stable/settings. Documentation for
+# custom Taiga settings can be found at https://taigaio.github.io/taiga-doc/dist/.
+
+#########################################
+## GENERIC
+#########################################
+
+#ALLOWED_HOSTS = [
+#    'localhost',
+#]
+
+#ADMINS = (
+#    ("Admin", "example@example.com"),
+#)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'taiga',
+        'USER': 'taiga',
+        'PASSWORD': 'changeme',
+        'HOST': 'database',
+        'PORT': '5432',
+    }
+}
+
+# The absolute url is mandatory because attachments
+# urls depends on it. In production should be set
+# something like https://media.taiga.io/.
+#MEDIA_URL = "http://localhost:8080/media/"
+#STATIC_URL = "http://localhost:8080/static/"
+
+# DO NOT FORGET TO SET THIS!!!
+# You can use this command to generate the key:
+# $ python <<EOF
+# from django.core.management.utils import get_random_secret_key
+# print(get_random_secret_key())
+# EOF
+#SECRET_KEY = ''
+
+#SITES = {
+#    "api": {
+#       "scheme": "http",
+#       "domain": "localhost:8080",
+#       "name": "api"
+#    },
+#    "front": {
+#       "scheme": "http",
+#       "domain": "localhost:8080",
+#       "name": "front"
+#    },
+#}
+
+#SITE_ID = "api"
+
+
+#########################################
+## THROTTLING
+#########################################
+
+#REST_FRAMEWORK["DEFAULT_THROTTLE_RATES"] = {
+#    "anon-write": "20/min",
+#    "user-write": None,
+#    "anon-read": None,
+#    "user-read": None,
+#    "import-mode": None,
+#    "import-dump-mode": "1/minute",
+#    "create-memberships": None,
+#    "login-fail": None,
+#    "register-success": None,
+#    "user-detail": None,
+#    "user-update": None,
+#}
+
+# This list should contain:
+#  - Taiga users IDs
+#  - Valid clients IP addresses (X-Forwarded-For header)
+#REST_FRAMEWORK["DEFAULT_THROTTLE_WHITELIST"] = []
+
+
+#########################################
+## MAIL SYSTEM SETTINGS
+#########################################
+
+#DEFAULT_FROM_EMAIL = "john@doe.com"
+#CHANGE_NOTIFICATIONS_MIN_INTERVAL = 300 #seconds
+
+# EMAIL SETTINGS EXAMPLE
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_USE_TLS = False
+#EMAIL_USE_SSL = False # You cannot use both (TLS and SSL) at the same time!
+#EMAIL_HOST = 'localhost'
+#EMAIL_PORT = 25
+#EMAIL_HOST_USER = 'user'
+#EMAIL_HOST_PASSWORD = 'password'
+
+# GMAIL SETTINGS EXAMPLE
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_USE_TLS = True
+#EMAIL_HOST = 'smtp.gmail.com'
+#EMAIL_PORT = 587
+#EMAIL_HOST_USER = 'youremail@gmail.com'
+#EMAIL_HOST_PASSWORD = 'yourpassword'
+
+
+#########################################
+## REGISTRATION
+#########################################
+
+#PUBLIC_REGISTER_ENABLED = True
+
+# LIMIT ALLOWED DOMAINS FOR REGISTER AND INVITE
+# None or [] values in USER_EMAIL_ALLOWED_DOMAINS means allow any domain
+#USER_EMAIL_ALLOWED_DOMAINS = None
+
+# PUCLIC OR PRIVATE NUMBER OF PROJECT PER USER
+#MAX_PRIVATE_PROJECTS_PER_USER = None # None == no limit
+#MAX_PUBLIC_PROJECTS_PER_USER = None # None == no limit
+#MAX_MEMBERSHIPS_PRIVATE_PROJECTS = None # None == no limit
+#MAX_MEMBERSHIPS_PUBLIC_PROJECTS = None # None == no limit
+
+# GITHUB SETTINGS
+#GITHUB_URL = "https://github.com/"
+#GITHUB_API_URL = "https://api.github.com/"
+#GITHUB_API_CLIENT_ID = "yourgithubclientid"
+#GITHUB_API_CLIENT_SECRET = "yourgithubclientsecret"
+
+
+#########################################
+## SITEMAP
+#########################################
+
+# If is True /front/sitemap.xml show a valid sitemap of taiga-front client
+#FRONT_SITEMAP_ENABLED = False
+#FRONT_SITEMAP_CACHE_TIMEOUT = 24*60*60  # In second
+
+
+#########################################
+## FEEDBACK
+#########################################
+
+# Note: See config in taiga-front too
+#FEEDBACK_ENABLED = True
+#FEEDBACK_EMAIL = "support@taiga.io"
+
+
+#########################################
+## STATS
+#########################################
+
+#STATS_ENABLED = False
+#FRONT_SITEMAP_CACHE_TIMEOUT = 60*60  # In second
+
+
+#########################################
+## CELERY
+#########################################
+# Set to True to enable celery and work in async mode or False
+# to disable it and work in sync mode. You can find the celery
+# settings in settings/celery.py and settings/celery-local.py
+#CELERY_ENABLED = True
+
+
+#########################################
+## IMPORTERS
+#########################################
+
+# Configuration for the GitHub importer
+# Remember to enable it in the front client too.
+#IMPORTERS["github"] = {
+#    "active": True, # Enable or disable the importer
+#    "client_id": "XXXXXX_get_a_valid_client_id_from_github_XXXXXX",
+#    "client_secret": "XXXXXX_get_a_valid_client_secret_from_github_XXXXXX"
+#}
+
+# Configuration for the Trello importer
+# Remember to enable it in the front client too.
+#IMPORTERS["trello"] = {
+#    "active": True, # Enable or disable the importer
+#    "api_key": "XXXXXX_get_a_valid_api_key_from_trello_XXXXXX",
+#    "secret_key": "XXXXXX_get_a_valid_secret_key_from_trello_XXXXXX"
+#}
+
+# Configuration for the Jira importer
+# Remember to enable it in the front client too.
+#IMPORTERS["jira"] = {
+#    "active": True, # Enable or disable the importer
+#    "consumer_key": "XXXXXX_get_a_valid_consumer_key_from_jira_XXXXXX",
+#    "cert": "XXXXXX_get_a_valid_cert_from_jira_XXXXXX",
+#    "pub_cert": "XXXXXX_get_a_valid_pub_cert_from_jira_XXXXXX"
+#}
+
+# Configuration for the Asane importer
+# Remember to enable it in the front client too.
+#IMPORTERS["asana"] = {
+#    "active": True, # Enable or disable the importer
+#    "callback_url": "{}://{}/project/new/import/asana".format(SITES["front"]["scheme"],
+#                                                              SITES["front"]["domain"]),
+#    "app_id": "XXXXXX_get_a_valid_app_id_from_asana_XXXXXX",
+#    "app_secret": "XXXXXX_get_a_valid_app_secret_from_asana_XXXXXX"
+#}
